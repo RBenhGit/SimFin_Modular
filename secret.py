44b6acce4d5acf6747eb1abed344a62242be0ef2
rbh <ran@benhur.co>
2025-05-17 02:45:07 +0300

    Remove unintended file

--- secret.py
+++
@@ -1,4 +0,0 @@
-# secrets.py
-# החלף את הערך הזה במפתח אקראי, חזק וייחודי משלך!
-# ניתן לייצר מפתח לדוגמה עם: python -c "import os; print(os.urandom(24).hex())"
-FLASK_SECRET_KEY ="12a0f2b5dcb736d24d186462e922dc3a11427f3da0d8583e"
